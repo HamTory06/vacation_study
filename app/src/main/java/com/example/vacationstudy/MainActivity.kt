cf8c96226b9fde492df34e315ac0f42ab84ca3ca
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-01-02 19:35:55 +0900

    카카오톡 로그인 따라 만들기

--- app/src/main/java/com/example/vacationstudy/MainActivity.kt
+++ app/src/main/java/com/example/vacationstudy/MainActivity.kt
@@ -4,26 +4,34 @@ import android.content.Context
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
+import android.view.View
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import com.example.vacationstudy.databinding.ActivityMainBinding
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val name = TextView(this).apply{ //람다식임 아님 말고
+        val binding = ActivityMainBinding.inflate(layoutInflater) //binding은 findviewByid대신 사용하는것
+        setContentView(binding.root)
+        binding.button.setOnClickListener{ //button_id를 가진 button을 클릭하면 Log로 "button on Click"출력
+            Log.d("tag","button on Click")
+        }
+        /*val name = TextView(this).apply{ //람다식임 아님 말고
             typeface = Typeface.DEFAULT_BOLD //글자 폰트 설정
             text = "HamTory"
         }
-/*        val name = TextView(this) apply안쓰면 수신객체를 적어야됨
+*//*        val name = TextView(this) apply안쓰면 수신객체를 적어야됨
         name.typeface = Typeface.DEFAULT_BOLD
-        name.text = "HamTory"*/
+        name.text = "HamTory"*//*
         val image = ImageView(this).also{ //also는 apply와 비슷하지만 also는 작업을 한뒤 return한다고 한다
             it.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.image)) //사진 가져오기
         }
@@ -39,6 +47,6 @@ class MainActivity : AppCompatActivity() {
             addView(address, WRAP_CONTENT, WRAP_CONTENT)
         }
         //LinearLayout 객체를 화면에 출력
-        setContentView(layout)
+        setContentView(layout)*/
     }
 }
