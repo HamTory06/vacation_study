b23bac9a17262894fb4fc7c784611b6597e5e0a0
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-01-02 21:08:20 +0900

    onTouchEvent공부중

--- ch6_view/src/main/java/com/example/ch6_view/MainActivity.kt
+++ ch6_view/src/main/java/com/example/ch6_view/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.ch6_view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.MotionEvent.ACTION_DOWN
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean { //MotionEvent는 터치의 종류와 발생 지점을 남긴다
+        return super.onTouchEvent(event)
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN{
+                Log.d("touch Event")
+            }
+        }
     }
 }
