32b6a9e73eeb0025b7ea53c66aa8861e98bf534a
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-01-10 14:26:16 +0900

    뷰 페이지2 리사이클러뷰 어댑터 이용 공부중 (프래그먼트에서 layout을 다 fragment_one으로 되있어서 잘되고 있었지만 잘못된줄 알고 계속 찾고 있었다;; 삽질)

--- jetpack_viewpager2_fragmentstateadapter/src/main/java/com/example/jetpack_viewpager2_fragmentstateadapter/MainActivity.kt
+++ jetpack_viewpager2_fragmentstateadapter/src/main/java/com/example/jetpack_viewpager2_fragmentstateadapter/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.adapter.FragmentViewHolder
 import androidx.viewpager2.widget.ViewPager2
 import com.example.jetpack_viewpager2_fragmentstateadapter.databinding.ActivityMainBinding
 
@@ -17,19 +18,27 @@ class MainActivity : AppCompatActivity() {
         mbinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val fragmentList = listOf(OneFragment(),TwoFragment(),ThreeFragment())
+
         val adapter = MyFragmentPagerAdapter(this)
         adapter.fragment = fragmentList
         binding.viewpager.adapter = adapter
-        binding.viewpager.orientation = ViewPager2.ORIENTATION_VERTICAL //뷰 페이저2를 세로로 적용
+        //binding.viewpager.orientation = ViewPager2.ORIENTATION_VERTICAL //뷰 페이저2를 세로로 적용
     }
 }
 
 class MyFragmentPagerAdapter(activity: FragmentActivity): FragmentStateAdapter(activity){
 
-    var fragment = listOf<Fragment>()
+    var fragment: List<Fragment>
+    init{
+        fragment = listOf(OneFragment(),TwoFragment(),ThreeFragment())
+        Log.d("상태","fragment size : ${fragment.size}")
+    }
 
     override fun getItemCount(): Int = fragment.size
 
-    override fun createFragment(position: Int): Fragment = fragment[position]
+    override fun createFragment(position: Int): Fragment{
+        Log.d("상태","fragment : $position")
+        return fragment[position]
+    }
 
 }
--- jetpack_viewpager2_fragmentstateadapter/src/main/java/com/example/jetpack_viewpager2_fragmentstateadapter/TwoFragment.kt
+++ jetpack_viewpager2_fragmentstateadapter/src/main/java/com/example/jetpack_viewpager2_fragmentstateadapter/TwoFragment.kt
@@ -19,7 +19,7 @@ class TwoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false)
+        return inflater.inflate(R.layout.fragment_two, container, false)
     }
 
 }
