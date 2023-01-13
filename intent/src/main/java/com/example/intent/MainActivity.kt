623271adce1f0aa144a40cd6f3ea693d9ef5b6b5
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-01-14 00:31:58 +0900

    ActivityResultLauncher

--- intent/src/main/java/com/example/intent/MainActivity.kt
+++ intent/src/main/java/com/example/intent/MainActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.intent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,21 +14,30 @@ class MainActivity : AppCompatActivity() {
     private val binding get() = mbinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mbinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        val intent: Intent = Intent(this,DetailActivity::class.java)
+        val requsetLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult())
+        {
+            val resultData = it.data?.getStringExtra("result")
+        }
         val intent: Intent = Intent(this,DetailActivity::class.java)
+        requsetLauncher.launch(intent)
         intent.putExtra("data1","Hello")
-        intent.putExtra("data2","World")
-        intent.putExtra("data3",10)
-        startActivity(intent)
+        intent.putExtra("data2",10)
+        binding.button.setOnClickListener {
+//            Log.d("상태","button")
+//            startActivity(intent) //사후 처리가 필요 없으면
+//            startActivityForResult(intent,10) //사후 처리가 필요하다면
+        }
     }
 
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) { //결과를 돌려받은 후 처리
-//        super.onActivityResult(requestCode, resultCode, data)
-//        if(requestCode == 10 && resultCode == Activity.RESULT_OK){
-//            val result = data?.getStringExtra("resultData")
-//            if (result != null) {
-//                Log.d("MainActivity상태",result)
-//            }
-//        }
-//    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 10 && resultCode == Activity.RESULT_OK){
+            val result = data?.getStringExtra("resultData")
+                Log.d("상태","result : $result")
+        }
+    }
 }
