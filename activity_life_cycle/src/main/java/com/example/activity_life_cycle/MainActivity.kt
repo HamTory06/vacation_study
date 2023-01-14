81efe1ab71481ccf3737301373dd9b8a4af4bd96
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-01-14 19:48:13 +0900

    onSaveInstanceState() 데이터 저장

--- activity_life_cycle/src/main/java/com/example/activity_life_cycle/MainActivity.kt
+++ activity_life_cycle/src/main/java/com/example/activity_life_cycle/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.activity_life_cycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +27,19 @@ class MainActivity : AppCompatActivity() {
         Log.d("상태","onPause()")
     }
 
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) { //이 함수가 호출된다는 것은 액티비티가 종료된다는 의미이다, Bundle에 데이터를 담아주면 자동으로 데이터를 파일로 저장해준다
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putString("data1","hello")
+        outState.putInt("data2",0)
+        Log.d("상태","onSaveInstanceState()")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val data1 = savedInstanceState.getString("data1")
+        val data2 = savedInstanceState.getInt("data2")
+    }
+
     override fun onStop() { //액티비티가 더 이상 표시되지 않음
         super.onStop()
         Log.d("상태","onStop()")
@@ -36,7 +50,4 @@ class MainActivity : AppCompatActivity() {
         Log.d("상태","onDestroy()")
     }
 
-    override fun onRestart() { //사용자가 액티비티를 탐색함
-        super.onRestart()
-    }
 }
