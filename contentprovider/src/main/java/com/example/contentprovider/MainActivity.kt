557fb1a2ce68d57b6931d75edba98c67dcc42c70
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-01-27 21:54:52 +0900

    OOM 문제 해결

--- contentprovider/src/main/java/com/example/contentprovider/MainActivity.kt
+++ contentprovider/src/main/java/com/example/contentprovider/MainActivity.kt
@@ -2,16 +2,19 @@ package com.example.contentprovider
 
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.contentprovider.databinding.ActivityMainBinding
+import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,44 +28,51 @@ class MainActivity : AppCompatActivity() {
         mbinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 //        requestLauncher.launch(intent)
-
-        val status = ContextCompat.checkSelfPermission(this, "android.permission.READ_CONTACTS")
-        if(status == PackageManager.PERMISSION_GRANTED){
-            //퍼미션 허용이라면
-            Log.d("상태","permission granted")
-        } else {
-            //퍼미션 요청 다이얼로그 표시
-            ActivityCompat.requestPermissions(this, arrayOf<String>("android.permission.READ_CONTACTS"),100)
-            Log.d("상태","permission denied")
-        }
-
-        requestLauncher = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult())
-        {
-            if (it.resultCode == RESULT_OK){
-                Log.d("상태","RESULT_OK")
-                val cursor = contentResolver.query(
-                    it!!.data!!.data!!,
-                    arrayOf<String>(
-                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-                        ContactsContract.CommonDataKinds.Phone.NUMBER
-                    ),
-                    null,
-                    null,
-                    null,
-                )
-                Log.d("상태태","cursor size....${cursor?.count}")
-                if(cursor!!.moveToFirst()){
-                    val name = cursor?.getString(0)
-                    val phone = cursor?.getString(1)
-                    binding.resultContact.text = "name : $name, phone : $phone"
-                }
-            }
-        }
+        val option = BitmapFactory.Options()
+        option.inSampleSize = 4
+        val Bitmap = BitmapFactory.decodeStream(InputStream.nullInputStream(), null, option)
         binding.button.setOnClickListener {
-            val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
-            requestLauncher.launch(intent)
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            intent.type = "image/*"
+            startActivity(intent)
         }
+//        val status = ContextCompat.checkSelfPermission(this, "android.permission.READ_CONTACTS")
+//        if(status == PackageManager.PERMISSION_GRANTED){
+//            //퍼미션 허용이라면
+//            Log.d("상태","permission granted")
+//        } else {
+//            //퍼미션 요청 다이얼로그 표시
+//            ActivityCompat.requestPermissions(this, arrayOf<String>("android.permission.READ_CONTACTS"),100)
+//            Log.d("상태","permission denied")
+//        }
+
+//        requestLauncher = registerForActivityResult(
+//            ActivityResultContracts.StartActivityForResult())
+//        {
+//            if (it.resultCode == RESULT_OK){
+//                Log.d("상태","RESULT_OK")
+//                val cursor = contentResolver.query(
+//                    it!!.data!!.data!!,
+//                    arrayOf<String>(
+//                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+//                        ContactsContract.CommonDataKinds.Phone.NUMBER
+//                    ),
+//                    null,
+//                    null,
+//                    null,
+//                )
+//                Log.d("상태태","cursor size....${cursor?.count}")
+//                if(cursor!!.moveToFirst()){
+//                    val name = cursor?.getString(0)
+//                    val phone = cursor?.getString(1)
+//                    binding.resultContact.text = "name : $name, phone : $phone"
+//                }
+//            }
+//        }
+//        binding.button.setOnClickListener {
+//            val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
+//            requestLauncher.launch(intent)
+//        }
     }
 
     override fun onRequestPermissionsResult(
@@ -77,5 +87,29 @@ class MainActivity : AppCompatActivity() {
         } else {
             Log.d("상태","permission denied")
         }
+    }
+
+    private fun calcuateInSampleSize(fileUri: Uri, reqWidth: Int, reqHeight: Int): Int{
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        try {
+            var inputStream = contentResolver.openInputStream(fileUri)
+            BitmapFactory.decodeStream(inputStream, null, options)
+            inputStream!!.close()
+            inputStream = null
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        val (height: Int, width: Int) = options.run { outHeight to outWidth }
+        var inSampleSize = 1
+        //inSampleSize 비울 계산
+        if(height > reqHeight || width > reqWidth){
+            val halfHeight: Int = height / 2
+            val halfWidth: Int = width / 2
+            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth){
+                inSampleSize *= 2
+            }
+        }
+        return inSampleSize
     }
 }
