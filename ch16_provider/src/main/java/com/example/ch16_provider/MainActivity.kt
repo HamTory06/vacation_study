a35d9c614086780aab33a5fb0628c5d8b8dfc573
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-01-29 22:18:40 +0900

    ch16 카메라로 사진 찍고 -> 갤러리에서 사진 선택 -> 사진 띄우기

---
+++ ch16_provider/src/main/java/com/example/ch16_provider/MainActivity.kt
@@ -0,0 +1,133 @@
+package com.example.ch16_provider
+
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
+import com.example.ch16_provider.databinding.ActivityMainBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var mbinding: ActivityMainBinding ?= null
+    private val binding get() = mbinding!!
+
+    lateinit var filePath: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mbinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //갤러리 요청 런처
+        val requsetGalleryLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult())
+        {
+            try {
+                //inSampleSize 비율 계산, 지정
+                val calRatio = calculateInSampleSize(
+                    it.data!!.data!!,
+                    resources.getDimensionPixelSize(R.dimen.imgSize),
+                    resources.getDimensionPixelSize(R.dimen.imgSize)
+                )
+                val option = BitmapFactory.Options()
+                option.inSampleSize = calRatio
+                //이미지 로딩
+                var inputStream = contentResolver.openInputStream(it.data!!.data!!)
+                val bitmap = BitmapFactory.decodeStream(inputStream, null, option)
+                inputStream!!.close()
+                inputStream = null
+                bitmap?.let {
+                    binding.userImageView.setImageBitmap(bitmap)
+                } ?: let {
+                    Log.d("상태","bitmap null")
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        binding.galleryButton.setOnClickListener {
+            //갤러리 앱
+            val intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            intent.type = "image/*"
+            requsetGalleryLauncher.launch(intent)
+        }
+
+        //카메라 요청 런처
+        val requestCameraFileLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult())
+        {
+            //카메라 앱
+            val calRatio = calculateInSampleSize(
+                Uri.fromFile(File(filePath)),
+                resources.getDimensionPixelSize(R.dimen.imgSize),
+                resources.getDimensionPixelSize(R.dimen.imgSize)
+            )
+            val option = BitmapFactory.Options()
+            option.inSampleSize = calRatio
+            val bitmap = BitmapFactory.decodeFile(filePath, option)
+            bitmap?.let{
+                binding.userImageView.setImageBitmap(bitmap)
+            }
+        }
+
+        binding.cameraButton.setOnClickListener {
+            //카메라 앱
+            val timeStamp: String =
+                SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+            val storageDir: File ?=
+                getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            val file = File.createTempFile(
+                "JPEG_${timeStamp}_",
+                ".JPG",
+                storageDir
+            )
+            filePath = file.absolutePath
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this,
+                "com.example.ch15_provider.fileprovider",file
+            )
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            requestCameraFileLauncher.launch(intent)
+        }
+    }
+
+    private fun calculateInSampleSize(fileUri: Uri, reqWidth: Int, reqHeight: Int): Int{
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        try {
+            var inputStream = contentResolver.openInputStream(fileUri)
+
+            //inJustDecodeBounds 값을 true로 설정한 상태에서 decondeXXX() 를 호출.
+            //로딩 하고자 하는 이미지의 각종 정보가 options에 설정 된다.
+            BitmapFactory.decodeStream(inputStream, null, options)
+            inputStream!!.close()
+            inputStream = null
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        //비율 계산.....
+        val (height: Int, width: Int) = options.run { outHeight to outWidth }
+        var inSampleSize = 1
+        //inSampleSize 비율 계산
+        if(height > reqHeight || width > reqWidth){
+            val halfHeight: Int = height / 2
+            val halfWidth: Int = width / 2
+
+            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+                inSampleSize *= 2
+            }
+        }
+        return inSampleSize
+    }
+}
