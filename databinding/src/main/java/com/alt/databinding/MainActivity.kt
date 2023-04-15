e681a2d321af06a1bd719d010e6ad58933cf1b43
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-04-15 21:36:10 +0900

    databinding

--- databinding/src/main/java/com/alt/databinding/MainActivity.kt
+++ databinding/src/main/java/com/alt/databinding/MainActivity.kt
@@ -8,13 +8,22 @@ import androidx.databinding.DataBindingUtil
 import com.alt.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val binding: ActivityMainBinding by lazy { DataBindingUtil.setContentView(this, R.layout.activity_main) }
+    // xml 파일명이 카멜케이스로 클래스가 자동생성 됩니다.
+    private lateinit var binding: ActivityMainBinding
+
+    var text = "Hello!"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.editText.addTextChangedListener{
-            binding.textView.text = binding.editText.text
-            Log.d("상태","변경")
+        // binding 세팅
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        // 현재 binding시킨 xml의 variable name
+        binding.main = this
+        // binding 버튼 클릭 이벤트
+        binding.btnChange.setOnClickListener {
+            text = "Hello Binding!"
+            // Data가 변동될 경우 binding된 View들에 Data 변화를 알려줌
+            binding.invalidateAll()
         }
-
     }
 }
